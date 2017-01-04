30c5c1e26e38cd8e9f63189e4019c5f529532ec7
Tareq Mahmood <tareqtms@yahoo.com>
2017-01-04 20:41:20 +0600

    Authentication for pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def index
     @articles = Article.paginate(page:params[:page], per_page: 5)
   end
@@ -10,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article saved succesfully!"
       redirect_to article_path(@article)
@@ -49,6 +53,13 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    unless @article.user == current_user
+      flash[:danger] = "You can only edit / delete your own article!"
+      redirect_to article_path(@article)
+    end
   end
 
 end
